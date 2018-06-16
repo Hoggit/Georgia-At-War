204ca61c9232a3799227f28954f141fddf2ad27b
Jeremy Smitherman <jeremysmitherman@gmail.com>
2018-06-16 12:27:43 -0500

    Added better sam placement to avoid trees

--- init.lua
+++ init.lua
@@ -1,22 +1,22 @@
 -- Populate the world and gameplay environment.
 for i=1, 4 do
-    local zone_index = math.random(4)
+    local zone_index = math.random(23)
     local zone = ZONE:New("NorthSA6Zone" .. zone_index)
     RussianTheaterSA6Spawn:SpawnInZone(zone, true)
 end
 
 for i=1, 3 do
     if i < 3 then
-        local zone_index = math.random(3)
+        local zone_index = math.random(8)
         local zone = ZONE:New("NorthSA10Zone" .. zone_index)
         RussianTheaterSA10Spawn:SpawnInZone(zone, true)
     end
 
-    local zone_index = math.random(3)
+    local zone_index = math.random(8)
     local zone = ZONE:New("NorthSA10Zone" .. zone_index)
     RussianTheaterEWRSpawn:SpawnInZone(zone, true)
 
-    local zone_index = math.random(3)
+    local zone_index = math.random(8)
     local zone = ZONE:New("NorthSA10Zone" .. zone_index)
     RussianTheaterC2Spawn:SpawnInZone(zone, true)
 end
