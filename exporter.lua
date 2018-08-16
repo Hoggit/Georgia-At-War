eed2de7f53439968be2fcb6a746e4f92e879cb02
Jeremy Smitherman <jeremysmitherman@gmail.com>
2018-08-15 23:06:06 -0500

    Fixed lfs fs debacle

--- exporter.lua
+++ exporter.lua
@@ -1,5 +1,5 @@
 local write_state = function()
-    local stateFile = fs.writedir()..[[Scripts\GAW\state.json]]
+    local stateFile = lfs.writedir()..[[Scripts\GAW\state.json]]
     local fp = io.open(stateFile, 'w')
     fp:write(json:encode(game_state))
     fp:close()
