2013323be6ba228dd7ba517dca4b95a7e3a523b1
Jeremy Smitherman <jeremysmitherman@gmail.com>
2018-09-09 23:50:40 -0500

    Removed MOOSEY airbase gathering for initial state

--- GAW.lua
+++ GAW.lua
@@ -10,7 +10,6 @@ function log(str)
     if str == nil then str = 'nil' end
     if logFile then
        logFile:write("HOGGIT GAW LOG - " .. str .."\r\n")
-       BASE:I("HOGGIT GAW LOG - " .. str .."\r\n")
        logFile:flush()
     end
 end
@@ -38,11 +37,11 @@ game_state = {
         ["Russian Theater"] ={
             ["last_cap_spawn"] = 0,
             ["Airfields"] = {
-                [AIRBASE.Caucasus.Novorossiysk] = AIRBASE:FindByName(AIRBASE.Caucasus.Novorossiysk):GetCoalition(),
-                [AIRBASE.Caucasus.Gelendzhik] = AIRBASE:FindByName(AIRBASE.Caucasus.Gelendzhik):GetCoalition(),
-                [AIRBASE.Caucasus.Krymsk] = AIRBASE:FindByName(AIRBASE.Caucasus.Krymsk):GetCoalition(),
-                [AIRBASE.Caucasus.Krasnodar_Center] = AIRBASE:FindByName(AIRBASE.Caucasus.Krasnodar_Center):GetCoalition(),
-                [AIRBASE.Caucasus.Krasnodar_Pashkovsky] = AIRBASE:FindByName(AIRBASE.Caucasus.Krasnodar_Pashkovsky):GetCoalition()
+                ["Novorossiysk"] = Airbase.getByName("Novorossiysk"):getCoalition(),
+                ["Gelendzhik"] = Airbase.getByName("Gelendzhik"):getCoalition(),
+                ["Krymsk"] = Airbase.getByName("Krymsk"):getCoalition(),
+                ["Krasnodar-Center"] = Airbase.getByName("Krasnodar-Center"):getCoalition(),
+                ["Krasnodar-Pashkovsky"] = Airbase.getByName("Krasnodar-Pashkovsky"):getCoalition(),
             },
             ["Primary"] = {
                 ["Maykop-Khanskaya"] = false
@@ -62,11 +61,11 @@ game_state = {
             ["CTLD_ASSETS"] = {},
             ['Convoys'] ={},
             ["FARPS"] = {
-                ["SW Warehouse"] = AIRBASE:FindByName("SW Warehouse"):GetCoalition(),
-                ["NW Warehouse"] = AIRBASE:FindByName("NW Warehouse"):GetCoalition(),
-                ["SE Warehouse"] = AIRBASE:FindByName("SE Warehouse"):GetCoalition(),
-                ["NE Warehouse"] = AIRBASE:FindByName("NE Warehouse"):GetCoalition(),
-                ["MK Warehouse"] = AIRBASE:FindByName("MK Warehouse"):GetCoalition(),
+                ["SW Warehouse"] = Airbase.getByName("SW Warehouse"):getCoalition(),
+                ["NW Warehouse"] = Airbase.getByName("NW Warehouse"):getCoalition(),
+                ["SE Warehouse"] = Airbase.getByName("SE Warehouse"):getCoalition(),
+                ["NE Warehouse"] = Airbase.getByName("NE Warehouse"):getCoalition(),
+                ["MK Warehouse"] = Airbase.getByName("MK Warehouse"):getCoalition(),
             }
         }
     }
@@ -75,24 +74,24 @@ game_state = {
 log("Game State INIT")
 
 abslots = {
-    [AIRBASE.Caucasus.Novorossiysk] = {"Novoro Huey 1", "Novoro Huey 2", "Novoro Mi-8 1", "Novoro Mi-8 2"},
-    [AIRBASE.Caucasus.Gelendzhik] = {},
-    [AIRBASE.Caucasus.Krymsk] = {"Krymsk Gazelle M", "Krymsk Gazelle L", "Krymsk Huey 1", "Krymsk Huey 2", "Krymsk Mi-8 1", "Krymsk Mi-8 2"},
-    [AIRBASE.Caucasus.Krasnodar_Center] = {"Krasnador Huey 1", "Kras Mi-8 1", "Krasnador Huey 2", "Kras Mi-8 2"},
-    [AIRBASE.Caucasus.Krasnodar_Pashkovsky] = {"Krasnador2 Huey 1", "Kras2 Mi-8 1", "Krasnador2 Huey 2", "Kras2 Mi-8 2"},
+    ['Novorossiysk'] = {"Novoro Huey 1", "Novoro Huey 2", "Novoro Mi-8 1", "Novoro Mi-8 2"},
+    ['Gelendzhik'] = {},
+    ['Krymsk'] = {"Krymsk Gazelle M", "Krymsk Gazelle L", "Krymsk Huey 1", "Krymsk Huey 2", "Krymsk Mi-8 1", "Krymsk Mi-8 2"},
+    ['Krasnodar-Center'] = {"Krasnador Huey 1", "Kras Mi-8 1", "Krasnador Huey 2", "Kras Mi-8 2"},
+    ['Krasnodar_Pashkovsky'] = {"Krasnador2 Huey 1", "Kras2 Mi-8 1", "Krasnador2 Huey 2", "Kras2 Mi-8 2"},
     ['SW Warehouse'] = {"SWFARP Huey 1", "SWFARP Huey 2", "SWFARP Mi-8 1", "SWFARP Mi-8 2"},
     ['NW Warehouse'] = {"NWFARP Huey 1", "NWFARP Huey 2", "NWFARP Mi-8 1", "NWFARP Mi-8 2", "NWFARP KA50"},
-    ['SE Warehouse'] ={"SEFARP Gazelle M", "SEFARP Gazelle L", "SEFARP Huey 1", "SEFARP Huey 2", "SEFARP Mi-8 1", "SEFARP Mi-8 2", "SEFARP KA50"},
+    ['SE Warehouse'] = {"SEFARP Gazelle M", "SEFARP Gazelle L", "SEFARP Huey 1", "SEFARP Huey 2", "SEFARP Mi-8 1", "SEFARP Mi-8 2", "SEFARP KA50"},
     ['NE Warehouse'] = {"NEFARP Huey 1", "NEFARP Huey 2", "NEFARP Mi-8 1", "NEFARP Mi-8 2"},
     ['MK Warehouse'] = {"MKFARP Huey 1", "MKFARP Huey 2", "MKFARP Mi-8 1", "MKFARP Mi-8 2", "MK FARP Ka-50"},
 }
 
 logiSlots = {
-    [AIRBASE.Caucasus.Novorossiysk] = NovoLogiSpawn,
-    [AIRBASE.Caucasus.Gelendzhik] = nil,
-    [AIRBASE.Caucasus.Krymsk] = KryLogiSpawn,
-    [AIRBASE.Caucasus.Krasnodar_Center] = KrasCenterLogiSpawn,
-    [AIRBASE.Caucasus.Krasnodar_Pashkovsky] = KrasPashLogiSpawn,
+    ['Novorossiysk'] = NovoLogiSpawn,
+    ['Gelendzhik'] = nil,
+    ['Krymsk'] = KryLogiSpawn,
+    ['Krasnodar-Center'] = KrasCenterLogiSpawn,
+    ['Krasnodar-Pashkovsky'] = KrasPashLogiSpawn,
     ['MK Warehouse'] = MaykopLogiSpawn
 }
 
