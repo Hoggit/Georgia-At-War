f5ac00d8c72de7d562c10b4d406bed27e9669f9b
Lugghawk <acidictadpole@gmail.com>
2018-12-20 21:12:14 -0500

    utils unify

--- utils.lua
+++ utils.lua
@@ -9,12 +9,44 @@ function dump(o)
     else
        return tostring(o)
     end
- end
+end
 
- function split(s, delimiter)
+function split(s, delimiter)
     result = {};
     for match in (s..delimiter):gmatch("(.-)"..delimiter) do
         table.insert(result, match);
     end
     return result;
 end
+
+function randomFromList(list)
+  local idx = math.random(1, #list)
+  return list[idx]
+end
+
+function listContains(list, elem)
+  for _, value in ipairs(list) do
+    if value == elem then
+        return true
+    end
+  end
+
+  return false
+end
+
+tableIndex = function(tbl, val)
+  for i,v in pairs(tbl) do
+    if val == v then
+      return i
+    end
+  end
+end
+
+array_size = function(o)
+    local num = 0
+    for i, j in pairs(o) do
+        num = num + 1
+    end
+    return num
+end
+
