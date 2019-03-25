36bf38db55aa5ad583b09fab109b90840a897037
Gillogical <46956996+Gillogical@users.noreply.github.com>
2019-03-26 01:42:52 +1100

    Tanker cloud behaviour update (#3)
    * Tanker cloud behaviour update
    
    Added code to make the tankers avoid the cloud layers at the highest densities
    
    * Renamed to WeatherPositioning
    
    Cleaned up based on feedback
    
    * Update spawns.lua
    
    More cleanups
    
    Note that I've also bumped the speeds as the command sets desired Ground Speed, which at 18000' would probably cause aircraft like the mirage to fall out of the sky.
    
    I'll incorporate a TAS->IAS adjustment at a later date
    
    * Update spawns.lua

--- spawns.lua
+++ spawns.lua
@@ -125,6 +125,62 @@ NorthGeorgiaFARPTransportSpawns = {
     ["SE"] = {Spawner("SE FARP HELO"),nil, SEFARPLogiSpawn},
     ["MK"] = {Spawner("MK FARP HELO"), nil, MaykopLogiSpawn}
 }
+
+-- Tells a tanker (or other aircraft for that matter) to orbit between two points at a set altitude
+
+WeatherPositioning = {}
+WeatherPositioning.hMaxFlightAlt	= 5486	-- meters [18 000']: Don't let the aircraft fly higher than this as Hogs won't be able to refuel. TODO: Make overrideable
+WeatherPositioning.vSpeed			= 160		-- m/s [300kts]: Default speed of the unit
+WeatherPositioning.hClearance		= 305		-- meters [1000']: Default clearance to deconflict
+-- Realistically if we use this to deconflict tankers as well, we'd need 2000' as chicks approach them 1000' low
+
+-- Generic altitude deconfliction
+function WeatherPositioning.deconflictAltitude(altitudeIn, deconflictBase, deconflictTop)
+	local hAltitude = altitudeIn
+	if deconflictTop >= (altitudeIn - WeatherPositioning.hClearance) then
+		hAltitude = math.min(deconflictBase - WeatherPositioning.hClearance, altitudeIn)
+	end
+	return hAltitude
+end
+
+function WeatherPositioning.avoidCloudLayer(planeGroup, vSpeed_ms)
+	local planeGroupName	= planeGroup:getName()
+	vSpeed_ms = vSpeed_ms or WeatherPositioning.vSpeed
+	
+	-- Calculate orbit height. Ignore for partial cloud conditions
+	local clouds = env.mission.weather.clouds
+	local cloudBase = clouds.base
+	local cloudTop = clouds.base + clouds.thickness
+	local hOrbit = WeatherPositioning.hMaxFlightAlt
+	
+	if clouds.density >= 7 then 
+		hOrbit = WeatherPositioning.deconflictAltitude(hOrbit, cloudBase, cloudTop)
+	end
+	
+	local curRoute = mist.getGroupRoute(planeGroupName, true)
+	
+	for i = 1, #curRoute do
+		curRoute[i].alt = hOrbit
+		
+		-- Modify any orbit taskings
+		if #curRoute[i] ~= nil
+			 and #curRoute[i].task ~= nil
+			 and #curRoute[i].task.params ~= nil
+			 and #curRoute[i].task.params.tasks ~= nil then
+			
+			for _, curTask in pairs(curRoute[i].task.params.tasks) do
+				if curTask.id == "Orbit" then
+					curTask.params.altitude = hOrbit
+					curTask.params.speed = vSpeed_ms
+				end
+			end
+		end
+	end
+	
+	return mist.goRoute(planeGroup, curRoute)
+end
+
+
 scheduledSpawns = {}
 DestructibleStatics = {}
 DestroyedStatics = {}
@@ -132,11 +188,13 @@ DestroyedStatics = {}
 TexacoSpawn = Spawner("Texaco")
 TexacoSpawn:OnSpawnGroup(function(grp)
     scheduledSpawns[grp:getUnit(1):getName()] = {TexacoSpawn, 600}
+	WeatherPositioning.avoidCloudLayer(grp, 175) -- Init against cloud base at 175m/s (280 knots)
 end)
 
 ShellSpawn = Spawner("Shell")
 ShellSpawn:OnSpawnGroup(function(grp)
     scheduledSpawns[grp:getUnit(1):getName()] = {ShellSpawn, 600}
+	WeatherPositioning.avoidCloudLayer(grp, 195) -- Init against cloud base at 195m/s (380 knots)
 end)
 
 OverlordSpawn = Spawner("AWACS Overlord")
